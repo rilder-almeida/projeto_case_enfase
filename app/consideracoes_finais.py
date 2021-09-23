637b559cabc3f72871a1632e450d3cb565df6f4e
rilder-almeida <rilder.almeida@gmail.com>
2021-09-22 21:09:54 -0300

    final: deploy

--- app/consideracoes_finais.py
+++ app/consideracoes_finais.py
@@ -11,7 +11,7 @@ def consideracoes_finais():
 
         Primeiramente gostaria de agradecer a toda equipe da Ênfase Labs e Curso Ênfase, pela oportunidade,
         pelo processo seletivo bem elaborado e humanizado. Durante a case pude aprimorar minhas habilidades
-        com as tecnologias Pandas, Numpy, Jupyter, Python, Pandas Profiling, StreamLib e dentre outras. Devido ao tempo
+        com as tecnologias Pandas, Numpy, Jupyter, Python, Pandas Profiling, StreamLit e dentre outras. Devido ao tempo
         limitado, não consegui imprimir os padrões de projeto possuo, tendo que focar mais na entrega do resultado
         do que na elaboração. A gestão do tempo e planejamento foram softs kills que também pude praticar bastante. 
         Enfim, uma experiência interessantíssima. No mais, muito obrigado e até breve.
